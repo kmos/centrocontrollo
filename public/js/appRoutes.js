29c2953720eb797fd7e4f3f0ada53665da15f66b
mosfet <giovanni.panice@gmail.com>
2015-06-30 01:21:17 +0200

    AngularJS dir

---
+++ public/js/appRoutes.js
@@ -0,0 +1,21 @@
+// public/js/appRoutes.js
+    angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/nerds', {
+            templateUrl: 'views/ControlPanel.html',
+            controller: 'ControlPanelController'
+        });
+
+    $locationProvider.html5Mode(true);
+
+}]);
+
---
+++ public/js/controllers/ControlPanelCtrl.js
@@ -0,0 +1,8 @@
+// public/js/controllers/ControlPanelCtrl.js
+
+angular.module('ControlPanelCtrl', []).controller('ControlPanelController', function($scope) {
+
+    $scope.tagline = 'Nothing beats a pocket protector!';
+
+});
+
