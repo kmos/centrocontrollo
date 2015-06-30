fef0041886c482d752e18e94319bfbd82a569aea
mosfet <giovanni.panice@gmail.com>
2015-06-30 13:18:47 +0200

    add login routes

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,31 @@ module.exports = function(app) {
       res.json(nodes);
     });
   });
+  // =====================================
+  // LOGIN ===============================
+  // =====================================
+  // show the login form
+  app.get('/login', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    // res.render('login.ejs', { message: req.flash('loginMessage') }); 
+  });
+
+  // process the login form
+  // app.post('/login', do all our passport stuff here);
+
+  // =====================================
+  // SIGNUP ==============================
+  // =====================================
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    //res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
 
   app.get('*', function(req, res) {
     res.sendfile('./public/views/index.html');
