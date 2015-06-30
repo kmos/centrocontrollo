f25347c185a421268caba5cca320fd49c894c4b9
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-06-30 12:48:27 +0200

    Add example test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe('Example', function() {
+  describe('#example()', function() {
+    it('should be true', function() {
+      assert.equal(true, true);
+    });
+  });
+});
