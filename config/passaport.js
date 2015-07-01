54c0b758bc8574fc2e35df6e819eec39b3229538
mosfet <giovanni.panice@gmail.com>
2015-07-01 13:17:12 +0200

    add strategy passport auth

--- config/passaport.js
+++ config/passaport.js
@@ -1,24 +1,24 @@
-var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 //==================================================================
 // Define the strategy to be used by PassportJS
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if (username === "admin" && password === "porco") // stupid example
-      return done(null, {name: "admin"});
+module.exports = function(passport){
+  passport.use(new LocalStrategy(
+    function(username, password, done) {
+      if (username === "admin" && password === "porco") // stupid example
+        return done(null, {name: "admin"});
 
     return done(null, false, { message: 'Incorrect username.' });
-  }
-));
+    }
+  ));
 
-// Serialized and deserialized methods when got from session
-passport.serializeUser(function(user, done) {
+  // Serialized and deserialized methods when got from session
+  passport.serializeUser(function(user, done) {
     done(null, user);
-});
+  });
 
-passport.deserializeUser(function(user, done) {
+  passport.deserializeUser(function(user, done) {
     done(null, user);
-});
-
+  });
+};
 
