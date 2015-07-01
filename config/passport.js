bb15548a9929a397493347091895fd2863b86ac6
mosfet <giovanni.panice@gmail.com>
2015-07-01 18:58:44 +0200

    update to config file

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,12 @@ var LocalStrategy = require('passport-local').Strategy;
 
 //==================================================================
 // Define the strategy to be used by PassportJS
-module.exports = function(passport){
+module.exports = function(passport,config){
+  var authVal = config.get('System.credential');
   passport.use(new LocalStrategy(
     function(username, password, done) {
-      if (username === "admin" && password === "porco") // stupid example
+      //if (username === "admin" && password === "porco") // stupid example
+      if (username == authVal.username && password == authVal.password)  
         return done(null, {name: "admin"});
 
     return done(null, false, { message: 'Incorrect username.' });
