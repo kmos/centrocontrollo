6fbef5d8b9a89905cdfe6da5be22c031fc3539da
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-01 19:35:39 +0200

    Update node schema

--- app/models/node.js
+++ app/models/node.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 
+var Sensor = require('./sensor');
+
 var nodeSchema = new mongoose.Schema({
   id: String,
   address: String,
   connected: Boolean,
-  sensors: [],
+  sensors: [Sensor],
 });
 
 module.exports = mongoose.model('Node', nodeSchema);
