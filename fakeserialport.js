e86d6f84b9ce464e738b813accbf2c757fdf481e
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-02 13:27:52 +0200

    Make the fake serial port receive random measurements

--- fakeserialport.js
+++ fakeserialport.js
@@ -12,8 +12,14 @@ FakeSerialPort.prototype.open = function(callback) {
     this.eventHandlers["open"] && this.eventHandlers["open"]();
 
     setInterval((function() {
-      var buffer = new Buffer("aValue");
-      this.eventHandlers["data"] && this.eventHandlers["data"](buffer);
+      var measurement = {
+        nodeId: Math.floor(Math.random() * 100),
+        sensorId: Math.floor(Math.random() * 100),
+        timestamp: Date.now(),
+        value: Math.floor(Math.random() * 100),
+      };
+
+      this.eventHandlers["data"] && this.eventHandlers["data"](new Buffer(JSON.stringify(measurement)));
     }).bind(this), 5000);
   }).bind(this), 0);
 };
