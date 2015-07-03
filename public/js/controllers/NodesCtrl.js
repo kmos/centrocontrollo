82228014e88ffbed686add422c776cbe1f99e04d
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-03 23:27:20 +0200

    Remove commented out code

--- public/js/controllers/NodesCtrl.js
+++ public/js/controllers/NodesCtrl.js
@@ -1,12 +1,7 @@
-angular.module('NodesCtrl', []).controller('NodesController', ['$scope', '$http','Nodes', function($scope,$http, Nodes) {
-  //$scope.nodes = [
-  //  { ID: 0, address: '127.0.0.1', connected: 'Yes', },
-  //  { ID: 1, address: '127.0.0.2', connected: 'No', },
-  //];
-  Nodes.get()
-    .success(function(data){
-      $scope.nodes = data;
-      $scope.loading = false;
-    });
+angular.module('NodesCtrl', []).controller('NodesController', ['$scope', 'Nodes', function($scope, Nodes) {
+  Nodes.get().success(function(data){
+    $scope.nodes = data;
+    $scope.loading = false;
+  });
 }]);
 
