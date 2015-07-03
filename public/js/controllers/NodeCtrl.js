49b6afd0f381ffb9c9fbfeb9e028e43cdf26f9ea
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-04 01:07:47 +0200

    Open a sensor page from a node page. Fixes #2

--- public/js/controllers/NodeCtrl.js
+++ public/js/controllers/NodeCtrl.js
@@ -1,8 +1,12 @@
-angular.module('NodeCtrl', []).controller('NodeController', ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http) {
+angular.module('NodeCtrl', []).controller('NodeController', ["$scope", "$routeParams", "$http", "$location", function($scope, $routeParams, $http, $location) {
   $scope.nodeID = $routeParams.nodeID;
   $scope.sensors = [];
 
   $http.get("/api/nodes/" + $routeParams.nodeID).success(function(data) {
     $scope.sensors = data.sensors;
   });
+
+  $scope.setSelected = function(sensorID) {
+    $location.path("/Sensor/" + $routeParams.nodeID + "/" + sensorID);
+  };
 }]);
