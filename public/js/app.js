cac4c809f9e0c9e7970a5a3426e9a91d457621a4
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-04 03:22:35 +0200

    Add a directive to highlight the current active tab. Fixes #18

--- public/js/app.js
+++ public/js/app.js
@@ -11,4 +11,28 @@ angular.module('ccApp', [
   'NodeCtrl',
   'SensorCtrl',
   'LogoutCtrl',
+]).directive('highlightActiveTab', ['$location',
+  function($location) {
+    return {
+      link: function(scope, element, attrs) {
+        scope.$on("$routeChangeSuccess", function(event, current, previous) {
+          var firstLevelPath = $location.path().split('/')[1];
+
+          angular.forEach(element.children(), function(item) {
+            var li = angular.element(item)[0];
+            var a = li.children[0];
+
+            var tabLink = a.getAttribute('href').split('/')[1];
+
+            if (firstLevelPath === tabLink ||
+                (tabLink === "Nodes" && (firstLevelPath === "Node" || firstLevelPath === "Sensor"))) {
+              li.classList.add("active");
+            } else {
+              li.classList.remove("active");
+            }
+          })
+        });
+      }
+    };
+  }
 ]);
