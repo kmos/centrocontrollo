3f8671b3d0878d7a2a7f95ebffad92ec33dadcab
mosfet <giovanni.panice@gmail.com>
2015-07-04 17:04:53 +0200

    add commented https

--- server.js
+++ server.js
@@ -11,18 +11,30 @@ var session        = require('express-session');
 var cookieParser   = require('cookie-parser');
 var flash          = require('connect-flash');
 var config         = require('config');
+var http           = require('http');
+var https          = require('https');
+var fs             = require('fs');
+var crypto         = require('crypto');
 // configuration ===========================================
-    
-// config files
+var logs = require("./config/logger")(module);
 require('./config/passport')(passport, config); // passport config
-var dbConfig = config.get('System.dbConfig');
-
-// set our port
-var port = process.env.PORT || 8080; 
-
+var dbConfig = config.get('System.dbConfig'); //dbConfig
+var servConfig = config.get('System.servConfig'); //servConfig
+// set our port and address
+var HTTPport = servConfig.http_port;
+var HTTPSport = servConfig.https_port;
+var privatekey = servConfig.private_key;
+var certificate = servConfig.certificate;
+var ip_add = servConfig.ip;
 // connect to our mongoDB database 
 mongoose.connect(dbConfig.address);
-
+var conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error: '));
+//Https Keys and certificate
+var op = {
+  key: fs.readFileSync(servConfig.private_key, 'utf8'),
+  cert: fs.readFileSync(servConfig.certificate, 'utf8')
+};
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
 app.use(bodyParser.json()); 
@@ -60,7 +72,24 @@ require('./app/routes')(app,passport); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);
+app.listen(HTTPport);
+
+// Start the server
+/*
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.        
+  logs.info('Connect to Mongodb on %s', dbConfig.address);
+  //Http server
+  var HTTPserver = http.createServer(app).listen(HTTPport, ip_add, function() {
+   logs.info('Http Server is running on port: %d - ip address: %s', HTTPport, ip_add);
+  });      
+  //Https server to correctly run use root privilege
+  var HTTPSserver = https.createServer(op, app).listen(HTTPSport, function(){
+    logs.info('Https Server is running on port: %d - ip address: %s', HTTPSport, ip_add);
+  }); 
+});
+*/
+
 
 // expose app           
 exports = module.exports = app; 
