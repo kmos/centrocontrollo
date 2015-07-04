e2dcb66f083c5ee60c846089043db2e20298c65a
mosfet <giovanni.panice@gmail.com>
2015-07-04 14:50:54 +0200

    add winston config

---
+++ config/logger.js
@@ -0,0 +1,20 @@
+var winston = require('winston');
+
+function getLogger(module) {
+    var path = module.filename.split('/').slice(-2).join('/'); //using filename in log statements
+    
+    return new winston.Logger({
+        transports : [
+            new winston.transports.Console({
+                colorize:   true,
+                //level:      'debug',
+                label:      path
+            }),
+            new winston.transports.File({
+                filename: './log/node.log'
+            })
+        ]
+    });
+}
+
+module.exports = getLogger;
