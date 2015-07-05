4e73114cf60ab261d60ac73c09a2e7cc802e54da
mosfet <giovanni.panice@gmail.com>
2015-07-05 03:03:58 +0200

    change welcome message

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,7 +1,7 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-    $scope.tagline = 'To the moon and back!';   
+    $scope.tagline = 'nel Pannello di Controllo rete di sensori';   
 
 });
 
