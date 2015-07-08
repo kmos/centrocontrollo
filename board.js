bb21b53ca167140d2458cad5101a266388a25836
Marco Castelluccio <mar.castelluccio@studenti.unina.it>
2015-07-08 03:06:54 +0200

    Use the new binary format to communicate with the board

--- board.js
+++ board.js
@@ -1,12 +1,16 @@
 var config     = require('config');
 var SerialPort = require('./fakeserialport');
+var Node       = require('./app/models/node');
+
+const OFFSET_TYPE = 0;
+const OFFSET_START = 1;
+const READDATA_PACKET_TYPE = 0x00;
+const DATA_PACKET_TYPE = 0x02;
 
 var Board = function() {
   this.eventListeners = {};
 
-  var callListeners = (function(data) {
-    var msg = JSON.parse(data);
-
+  var callListeners = (function(msg) {
     if (!this.eventListeners[msg.type]) {
       return;
     }
@@ -32,6 +36,41 @@ var Board = function() {
     });
   }).bind(this);
 
+  var receiveMessage = function(buffer) {
+    var packetType = buffer.readUInt8(OFFSET_TYPE);
+
+    if (packetType === DATA_PACKET_TYPE) {
+      var nodeAddress = buffer.readUInt16BE(OFFSET_START);
+
+      // XXX: Fix callers to expect a node address instead of a node ID.
+      Node.find({
+        address: nodeAddress,
+      }, function(err, nodes) {
+        if (err) {
+          console.log("Error while retrieving node: " + err);
+          return;
+        }
+
+        if (nodes.length === 0) {
+          console.log("No nodes found with address = " + nodeAddress);
+          return;
+        }
+
+        callListeners({
+          type: "measurement",
+          nodeId: nodes[0]._id,
+          sensorId: buffer.readUInt8(OFFSET_START + 2),
+          timestamp: buffer.readUInt32BE(OFFSET_START + 3),
+          value: buffer.readUInt32BE(OFFSET_START + 7),
+          alarm: buffer.readUInt8(OFFSET_START + 11),
+        });
+      });
+    } else {
+      console.log("RECEIVED UNSUPPORTED MESSAGE");
+      console.log(buffer);
+    }
+  };
+
   this.serialPort = new SerialPort(config.get('System.serial.port'), {
     baudrate: config.get('System.serial.baudrate')
   }, false);
@@ -42,9 +81,7 @@ var Board = function() {
       return;
     }
 
-    this.serialPort.on('data', function(data) {
-      callListeners(data);
-    });
+    this.serialPort.on('data', receiveMessage);
   }).bind(this));
 };
 
@@ -83,13 +120,28 @@ Board.prototype.removeListener = function(callback, what, nodeId, sensorId) {
 };
 
 Board.prototype.askForMeasurement = function(nodeID, sensorID, callback) {
-  var message = JSON.stringify({
-    type: "measurement",
-    nodeID: nodeID,
-    sensorID: sensorID,
-  });
+  // XXX: Fix callers to use a node address instead of a node ID.
+  Node.find({
+    _id: nodeID,
+  }, (function(err, nodes) {
+    if (err) {
+      console.log("Error while retrieving node: " + err);
+      return;
+    }
 
-  this.serialPort.write(message, callback);
+    if (nodes.length === 0) {
+      console.log("No nodes found with ID = " + nodeID);
+      return;
+    }
+
+    var buffer = new Buffer(OFFSET_START + 3);
+
+    buffer.writeUInt8(READDATA_PACKET_TYPE, OFFSET_TYPE);
+    buffer.writeUInt16BE(nodes[0].address, OFFSET_START);
+    buffer.writeUInt8(sensorID, OFFSET_START + 2);
+
+    this.serialPort.write(buffer, callback);
+  }).bind(this));
 };
 
 Board.prototype.replyCanJoin = function(nodeID, reply, callback) {
@@ -100,6 +152,6 @@ Board.prototype.replyCanJoin = function(nodeID, reply, callback) {
   });
 
   this.serialPort.write(message, callback);
-}
+};
 
 module.exports = new Board();
--- fakeserialport.js
+++ fakeserialport.js
@@ -1,3 +1,21 @@
+const OFFSET_TYPE = 0;
+const OFFSET_START = 1;
+const READDATA_PACKET_TYPE = 0x00;
+const DATA_PACKET_TYPE = 0x02;
+
+function buildMeasurementPacket(nodeAddress, sensorID, alarm) {
+  var reply = new Buffer(OFFSET_START + 12);
+
+  reply.writeUInt8(DATA_PACKET_TYPE, OFFSET_TYPE);
+  reply.writeUInt16BE(nodeAddress, OFFSET_START);
+  reply.writeUInt8(sensorID, OFFSET_START + 2);
+  reply.writeUInt32BE(Date.now() | 0, OFFSET_START + 3);
+  reply.writeUInt32BE(Math.floor(Math.random() * 100), OFFSET_START + 7);
+  reply.writeUInt8(alarm, OFFSET_START + 11);
+
+  return reply;
+}
+
 var FakeSerialPort = function(path, options) {
   this.eventHandlers = {};
 };
@@ -13,59 +31,51 @@ FakeSerialPort.prototype.open = function(callback) {
 
     // Send a fake measurement every 5 seconds
     setInterval((function() {
-      var message = {
-        type: "measurement",
-        nodeId: Math.floor(Math.random() * 5),
-        sensorId: Math.floor(Math.random() * 5),
-        timestamp: Date.now(),
-        value: Math.floor(Math.random() * 100),
-        alarm: false,
-      };
+      var message = buildMeasurementPacket(Math.floor(Math.random() * 5),
+                                           Math.floor(Math.random() * 5),
+                                           0);
 
-      this.eventHandlers["data"] && this.eventHandlers["data"](new Buffer(JSON.stringify(message)));
+      this.eventHandlers["data"] && this.eventHandlers["data"](message);
     }).bind(this), 5000);
 
     // Send a fake alarm every 10 seconds
     setInterval((function() {
-      var message = {
-        type: "measurement",
-        nodeId: Math.floor(Math.random() * 5),
-        sensorId: Math.floor(Math.random() * 5),
-        timestamp: Date.now(),
-        value: Math.floor(Math.random() * 100),
-        alarm: true,
-      };
+      var message = buildMeasurementPacket(Math.floor(Math.random() * 5),
+                                           Math.floor(Math.random() * 5),
+                                           1);
 
-      this.eventHandlers["data"] && this.eventHandlers["data"](new Buffer(JSON.stringify(message)));
+      this.eventHandlers["data"] && this.eventHandlers["data"](message);
     }).bind(this), 10000);
 
     // Send a fake canJoin request every 20 seconds
-    setInterval((function() {
+    /*setInterval((function() {
       var message = {
         type: "canJoin",
         nodeID: Math.floor(Math.random() * 100),
       };
 
       this.eventHandlers["data"] && this.eventHandlers["data"](new Buffer(JSON.stringify(message)));
-    }).bind(this), 20000);
+    }).bind(this), 20000);*/
   }).bind(this), 0);
 };
 
 FakeSerialPort.prototype.write = function(buffer, callback) {
-  setTimeout((function() {
-    var message = JSON.parse(buffer);
+  var packetType = buffer.readUInt8(OFFSET_TYPE);
 
-    var reply;
+  var reply;
+
+  if (packetType === READDATA_PACKET_TYPE) {
+    var nodeAddress = buffer.readUInt16BE(OFFSET_START);
+    var sensorID = buffer.readUInt8(OFFSET_START + 2);
+
+    reply = buildMeasurementPacket(nodeAddress, sensorID, Math.floor(Math.random() * 2));
+  }
+
+  setTimeout((function() {
+    /*var message = JSON.parse(buffer);
 
     if (message.type === "measurement") {
-      reply = {
-        type: "measurement",
-        nodeId: message.nodeID,
-        sensorId: message.sensorID,
-        timestamp: Date.now(),
-        value: Math.floor(Math.random() * 100),
-        alarm: Math.floor(Math.random() * 2) ? true : false,
-      };
+
     } else if (message.type === "canJoin") {
       if (message.reply === 1) {
         reply = {
@@ -75,10 +85,10 @@ FakeSerialPort.prototype.write = function(buffer, callback) {
       } else {
         console.log("noCanJoin!");
       }
-    }
+    }*/
 
     if (reply) {
-      this.eventHandlers["data"] && this.eventHandlers["data"](new Buffer(JSON.stringify(reply)));
+      this.eventHandlers["data"] && this.eventHandlers["data"](reply);
     }
 
     callback && callback();
